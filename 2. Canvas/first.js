bf304f828663405ce682f20158e41ecb0ccc8d01
NazarJS <NPaluyanau@gomel.iba.by>
2021-07-05 13:44:28 +0300

    syntax correction

--- 2. Canvas/first.js
+++ 2. Canvas/first.js
@@ -1,12 +1,10 @@
-const canvas = document.querySelector("#draw");
-const ctx = canvas.getContext("2d");
-// canvas.width = window.innerWidth;
-// canvas.height = window.innerHeight;
-ctx.strokeStyle = "silver";
-ctx.lineJoin = "round";
-ctx.lineCap = "round";
+const canvas = document.querySelector('#draw');
+const ctx = canvas.getContext('2d');
+const lineType = 'round';
+ctx.strokeStyle = 'silver';
+ctx.lineJoin = lineType;
+ctx.lineCap = lineType;
 ctx.lineWidth = 10;
-// ctx.globalCompositeOperation = 'multiply';
 
 let isDrawing = false;
 let lastX = 0;
@@ -14,55 +12,52 @@ let lastY = 0;
 let hue = 0;
 let direction = true;
 
-function draw(e) {
+function draw(event) {
   if (!isDrawing) return; // stop the fn from running when they are not moused down
-  console.log(e);
-  // ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
   ctx.beginPath();
   // start from
   ctx.moveTo(lastX, lastY);
   // go to
-  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.lineTo(event.offsetX, event.offsetY);
   ctx.stroke();
-  [lastX, lastY] = [e.offsetX, e.offsetY];
+  [lastX, lastY] = [event.offsetX, event.offsetY];
 }
 
-canvas.addEventListener("mousedown", (e) => {
+canvas.addEventListener('mousedown', (event) => {
   isDrawing = true;
-  [lastX, lastY] = [e.offsetX, e.offsetY];
+  [lastX, lastY] = [event.offsetX, event.offsetY];
 });
 
-canvas.addEventListener("mousemove", draw);
-canvas.addEventListener("mouseup", () => (isDrawing = false));
-canvas.addEventListener("mouseout", () => (isDrawing = false));
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mouseup', () => (isDrawing = false));
+canvas.addEventListener('mouseout', () => (isDrawing = false));
 
 let colorsArr = [
-  "#e74c3c",
-  "#8e44ad",
-  "#f1c40f",
-  "#e67e22",
-  "black",
-  "green",
-  "#B33771",
-  "#FC427B",
-  "red",
+  '#e74c3c',
+  '#8e44ad',
+  '#f1c40f',
+  '#e67e22',
+  'black',
+  'green',
+  '#B33771',
+  '#FC427B',
+  'red',
 ];
 
 for (i = 0; i < colorsArr.length; i++) {
-  let btn = document.createElement("BUTTON");
-  btn.value = colorsArr[i];
-  btn.style.backgroundColor = colorsArr[i];
-  btn.id = "but";
+  let btnOfColor = document.createElement('BUTTON');
+  btnOfColor.value = colorsArr[i];
+  btnOfColor.style.backgroundColor = colorsArr[i];
+  btnOfColor.id = 'but';
 
-  btn.onclick = () => {
-    ctx.strokeStyle = btn.value;
-    console.log(ctx.strokeStyle);
+  btnOfColor.onclick = () => {
+    ctx.strokeStyle = btnOfColor.value;
   };
 
-  document.getElementById("myDiv").appendChild(btn);
+  document.getElementById('myDiv').appendChild(btn);
 }
 
-let range = document.querySelector(".number");
+let range = document.querySelector('.number');
 range.oninput = function () {
   ctx.lineWidth = this.value;
 };
